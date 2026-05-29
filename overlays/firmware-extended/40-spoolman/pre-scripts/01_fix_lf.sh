ed1e593faec3d8e7fcfc5cc18b9b7c947e9a3051
n19htmares <smokin.rsx@gmail.com>
2026-05-28 19:34:33 -0700

    Replace patch 04 with idempotent Python script for print_task_config

--- overlays/firmware-extended/40-spoolman/pre-scripts/01_fix_lf.sh
+++ overlays/firmware-extended/40-spoolman/pre-scripts/01_fix_lf.sh
@@ -3,3 +3,4 @@ set -eo pipefail
 dos2unix "$ROOTFS_DIR/home/lava/klipper/klippy/extras/filament_protocol.py"
 dos2unix "$ROOTFS_DIR/home/lava/moonraker/moonraker/components/spoolman.py"
 dos2unix "$ROOTFS_DIR/home/lava/klipper/klippy/extras/print_task_config.py"
+python3 "$(dirname "$0")/02_add_spool_id_to_print_task.py"
