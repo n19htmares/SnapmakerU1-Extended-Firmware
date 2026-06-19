65501272db100cb1385b66a162b553953a23de23
n19htmares <smokin.rsx@gmail.com>
2026-06-18 18:32:43 -0700

    Replace fragile patch 01 with sed insertion for SPOOL_ID

--- overlays/firmware-extended/40-spoolman/pre-scripts/01_fix_lf.sh
+++ overlays/firmware-extended/40-spoolman/pre-scripts/01_fix_lf.sh
@@ -8,4 +8,6 @@ grep -q "filament_spool_id" "$F" || sed -i "s/    'filament_sku': \[0\] \* PHYSI
 grep -q "\['filament_spool_id'\]\[channel\]" "$F" || sed -i "s/tmp_print_task_config\['filament_sku'\]\[channel\] = info\['SKU'\]/tmp_print_task_config['filament_sku'][channel] = info['SKU']\n            tmp_print_task_config['filament_spool_id'][channel] = info['SPOOL_ID']/" "$F"
 grep -q "filament_spool_id = gcmd.get" "$F" || sed -i "s/filament_color_rgba = gcmd.get('FILAMENT_COLOR_RGBA', None)/filament_color_rgba = gcmd.get('FILAMENT_COLOR_RGBA', None)\n        filament_spool_id = gcmd.get('SPOOL_ID', None)/" "$F"
 grep -q "\['filament_spool_id'\]\[config_extruder\]" "$F" || sed -i "s/tmp_print_task_config\['filament_sku'\]\[config_extruder\] = 0/tmp_print_task_config['filament_sku'][config_extruder] = 0\n            tmp_print_task_config['filament_spool_id'][config_extruder] = filament_spool_id/" "$F"
+F2="$ROOTFS_DIR/home/lava/klipper/klippy/extras/filament_protocol.py"
+grep -q "'SPOOL_ID'" "$F2" || sed -i "s/'CARD_UID': 0,/'CARD_UID': 0,\n    'SPOOL_ID': 0,/" "$F2"
 echo "print_task_config.py patched"
